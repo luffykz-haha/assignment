14252cab81e3cdf116b47a87a331c6416f87920c
ryl-xy <ctxy19@gmail.com>
2026-04-21 18:54:23 +0800

    UI changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,11 +18,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <style>
-        body {
-            padding-bottom: 100px;
-        }
-    </style>
+
 </head>
 <body>
     <div id="app">
